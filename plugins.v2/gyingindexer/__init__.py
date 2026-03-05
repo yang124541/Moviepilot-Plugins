44689c311e676731720693a17fbccfa607437317
yang124541 <yang124541@users.noreply.github.com>
2026-03-05 12:30:32 +0800

    chore: 统一插件图标来源并升级到1.3.7

--- plugins.v2/gyingindexer/__init__.py
+++ plugins.v2/gyingindexer/__init__.py
@@ -20,8 +20,8 @@ from app.utils.string import StringUtils
 class GyingIndexer(_PluginBase):
     plugin_name = "观影（GYing）"
     plugin_desc = "为 GYing 提供磁力搜索与清晰度过滤支持。"
-    plugin_icon = "gying.png"
-    plugin_version = "1.3.6"
+    plugin_icon = "https://raw.githubusercontent.com/yang124541/moviepilot-plugin/main/gying.png"
+    plugin_version = "1.3.7"
     plugin_author = "yang124541"
     author_url = "https://github.com/yang124541/moviepilot-plugin"
     plugin_config_prefix = "gyingindexer_"
