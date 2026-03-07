1bd6322ff7c00f6523519e682d6419119f7b4135
yang124541 <yang124541@users.noreply.github.com>
2026-03-07 15:41:53 +0800

    chore(gying): 版本升级到1.4.4触发识别刷新

--- plugins.v2/gyingindexer/__init__.py
+++ plugins.v2/gyingindexer/__init__.py
@@ -21,7 +21,7 @@ class GyingIndexer(_PluginBase):
     plugin_name = "观影（GYing）"
     plugin_desc = "为 GYing 提供磁力搜索与清晰度过滤支持。"
     plugin_icon = "https://raw.githubusercontent.com/yang124541/moviepilot-plugin/main/gying.png"
-    plugin_version = "1.3.8"
+    plugin_version = "1.4.4"
     plugin_author = "yang124541"
     author_url = "https://github.com/yang124541/moviepilot-plugin"
     plugin_config_prefix = "gyingindexer_"
