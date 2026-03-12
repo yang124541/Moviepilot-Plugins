75ca55b23877c76330045d5749d00b1d3b1dbd13
yang124541 <yang124541@users.noreply.github.com>
2026-03-12 11:16:55 +0800

    fix(xunlei): 去掉顶部占位空白并发布1.7.9

--- plugins.v2/xunleihijackdownloader/__init__.py
+++ plugins.v2/xunleihijackdownloader/__init__.py
@@ -33,7 +33,7 @@ class XunleiHijackDownloader(_PluginBase):
     plugin_name = "迅雷下载接管"
     plugin_desc = "接管 MoviePilot 下载到迅雷，并可自动搬运到监控目录。"
     plugin_icon = "https://raw.githubusercontent.com/yang124541/moviepilot-plugin/main/xunlei.png"
-    plugin_version = "1.7.8"
+    plugin_version = "1.7.9"
     plugin_author = "yang124541"
     author_url = "https://github.com/yang124541/moviepilot-plugin"
     plugin_config_prefix = "xunleihijackdownloader_"
@@ -321,11 +321,11 @@ class XunleiHijackDownloader(_PluginBase):
         page: List[dict] = [
             {
                 "component": "VRow",
-                "props": {"class": "ma-0 pa-0", "noGutters": True, "style": "height:0;min-height:0;overflow:hidden;"},
+                "props": {"align": "center"},
                 "content": [
                     {
                         "component": "VCol",
-                        "props": {"cols": 12, "class": "ma-0 pa-0"},
+                        "props": {"cols": 12},
                         "content": [
                             {
                                 "component": "img",
@@ -341,19 +341,7 @@ class XunleiHijackDownloader(_PluginBase):
                                         interval_ms=1000,
                                     ),
                                 },
-                            }
-                        ],
-                    }
-                ],
-            },
-            {
-                "component": "VRow",
-                "props": {"align": "center"},
-                "content": [
-                    {
-                        "component": "VCol",
-                        "props": {"cols": 12},
-                        "content": [
+                            },
                             {
                                 "component": "VAlert",
                                 "props": {
