345fc421e81c1d6335f7370cf4a9f29c63a8a887
yang124541 <yang124541@users.noreply.github.com>
2026-03-12 21:36:12 +0800

    release(xunlei): bump version to 2.0.1

--- plugins.v2/xunleihijackdownloader/__init__.py
+++ plugins.v2/xunleihijackdownloader/__init__.py
@@ -34,7 +34,7 @@ class XunleiHijackDownloader(_PluginBase):
     plugin_name = "迅雷下载接管"
     plugin_desc = "接管 MoviePilot 下载到迅雷，并可自动搬运到监控目录。"
     plugin_icon = "https://raw.githubusercontent.com/yang124541/moviepilot-plugin/main/xunlei.png"
-    plugin_version = "1.9.31"
+    plugin_version = "2.0.1"
     plugin_author = "yang124541"
     author_url = "https://github.com/yang124541/moviepilot-plugin"
     plugin_config_prefix = "xunleihijackdownloader_"
@@ -576,16 +576,23 @@ class XunleiHijackDownloader(_PluginBase):
                                 {"component": "VCol", "props": {"cols": "auto", "class": "py-0 pr-4 d-flex align-center"}, "content": [image_node]},
                                 {
                                     "component": "VCol",
-                                    "props": {"class": "py-0", "style": "flex:1 1 auto;min-width:0;"},
+                                    "props": {"cols": 5, "md": 5, "class": "py-0", "style": "min-width:0;"},
                                     "content": [
                                         {
-                                            "component": "div",
+                                            "component": "VListItem",
                                             "props": {
-                                                "textContent": task_name,
+                                                "title": task_name,
+                                                "density": "compact",
                                                 "class": "px-0",
-                                                "style": "height:24px;line-height:24px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;",
+                                                "style": "padding-inline-start:0;padding-inline-end:0;",
                                             },
-                                        },
+                                        }
+                                    ],
+                                },
+                                {
+                                    "component": "VCol",
+                                    "props": {"cols": 4, "md": 4, "class": "py-0", "style": "min-width:0;"},
+                                    "content": [
                                         {
                                             "component": "div",
                                             "props": {
