0228369054bf13fd9c06705653c0b9d461da1b77
yang124541 <yang124541@users.noreply.github.com>
2026-03-30 08:26:57 +0800

    chore: update ldysg icon and package version

--- plugins.v2/ldysgindexer/__init__.py
+++ plugins.v2/ldysgindexer/__init__.py
@@ -30,7 +30,7 @@ class LdysgIndexer(_PluginBase):
     plugin_name = "老电影（ldysg）"
     plugin_desc = "为 ldysg.com 提供老旧电影磁力搜索支持，自动识别验证码。"
     plugin_icon = "https://raw.githubusercontent.com/yang124541/Moviepilot-Plugins/main/ldysg.png"
-    plugin_version = "1.3.5"
+    plugin_version = "1.3.6"
     plugin_author = "yang124541"
     author_url = "https://github.com/yang124541/moviepilot-plugin"
     plugin_config_prefix = "ldysgindexer_"
