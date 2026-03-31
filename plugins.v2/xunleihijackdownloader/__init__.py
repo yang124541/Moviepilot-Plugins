920588f2e7c6bf6edb8e78959b5be7067b6ad992
yang124541 <yang124541@users.noreply.github.com>
2026-03-31 21:59:08 +0800

    release: bump xunlei plugin to v2.4.2

--- plugins.v2/xunleihijackdownloader/__init__.py
+++ plugins.v2/xunleihijackdownloader/__init__.py
@@ -35,7 +35,7 @@ class XunleiHijackDownloader(_PluginBase):
     plugin_name = "迅雷下载接管"
     plugin_desc = "接管 MoviePilot 下载到迅雷，并可自动搬运到监控目录。"
     plugin_icon = "https://raw.githubusercontent.com/yang124541/moviepilot-plugin/main/xunlei.png"
-    plugin_version = "2.4.1"
+    plugin_version = "2.4.2"
     plugin_author = "yang124541"
     author_url = "https://github.com/yang124541/moviepilot-plugin"
     plugin_config_prefix = "xunleihijackdownloader_"
@@ -3303,7 +3303,8 @@ class XunleiHijackDownloader(_PluginBase):
                         renamed_src = self._resolve_movie_renamed_source_path(
                             source_root=source_root,
                             task_id=task_key,
-                        ) if task_key else None
+                            task_name=task_name,
+                        ) if (task_key or task_name) else None
                         if renamed_src and renamed_src.exists():
                             return False
                         return True
